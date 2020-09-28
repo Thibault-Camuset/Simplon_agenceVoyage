b7ded76b014ecc15d37a37bd8c6fe1d430607ba3
Thibault-Camuset <thibault.camuset@gmail.com>
2020-09-28 12:34:23 +0200

    modif API

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let pageContainer = document.getElementById('page-container');
 let page2Container = document.getElementById('page2-container');
 let button = document.getElementById('search-button');
-let adult= document.getElementById('input-adulte')
+let adult = document.getElementById('input-adulte')
 let divAdult = document.getElementById('adult')
 
 let adultValue;
@@ -9,13 +9,14 @@ let enfant = document.getElementById('input-enfant');
 let enfantParticipant = document.getElementById('participantEnfant');
 let enfantValue;
 
-let json=[];
-let airportsEurope=[];
+let json = [];
+let airportsEurope = [];
 let arrayAirportsList;
 
 
 
-button.addEventListener('click', ()=>{
+
+button.addEventListener('click', () => {
     pageContainer.classList.add('hidden');
     page2Container.classList.remove('hidden');
     adultValue = adult.value;
@@ -26,44 +27,44 @@ button.addEventListener('click', ()=>{
 
 
 
-function createAdultElement(){
+function createAdultElement() {
 
-    for(x=0;x<adultValue;x++){
+    for (x = 0; x < adultValue; x++) {
 
         let newAdult = document.createElement('div');
         newAdult.classList.add('adultElement')
 
-        let AdultName= document.createElement('input');
-        let AdultFirstName= document.createElement('input');
-        let AdultButton= document.createElement('input')
-        AdultName.type='text';
-        AdultName.placeholder='Nom'
-        AdultFirstName.type='text';
-        AdultFirstName.placeholder='Prénom'
-        AdultButton.type='radio';
-        
+        let AdultName = document.createElement('input');
+        let AdultFirstName = document.createElement('input');
+        let AdultButton = document.createElement('input')
+        AdultName.type = 'text';
+        AdultName.placeholder = 'Nom'
+        AdultFirstName.type = 'text';
+        AdultFirstName.placeholder = 'Prénom'
+        AdultButton.type = 'radio';
 
-        AdultName.classList.add('input','inputNameAdult');
-        AdultFirstName.classList.add('input','inputFirstNameAdult');
-        AdultButton.name='dirigeant';
+
+        AdultName.classList.add('input', 'inputNameAdult');
+        AdultFirstName.classList.add('input', 'inputFirstNameAdult');
+        AdultButton.name = 'dirigeant';
         AdultButton.classList.add('radio-button');
         newAdult.appendChild(AdultName);
         newAdult.appendChild(AdultFirstName);
         newAdult.appendChild(AdultButton);
         divAdult.appendChild(newAdult);
-        
+
     }
 }
 
 
-    
 
 
 
 
-function createEnfantElement (){
 
-    for(x=0; x<enfantValue; x++ ) {
+function createEnfantElement() {
+
+    for (x = 0; x < enfantValue; x++) {
 
         let newEnfant = document.createElement('div');
         newEnfant.classList.add('enfantElement');
@@ -96,7 +97,7 @@ function createEnfantElement (){
 //     let request = new XMLHttpRequest();
 //     request.onreadystatechange = function() {
 //         if (this.readyState == 4 && this.status == 200) {
-            
+
 //             json = JSON.parse(this.responseText);
 
 //             airportsList();
@@ -110,35 +111,62 @@ function createEnfantElement (){
 
 loadAirportsList();
 
+airportsList = [];
+
 function loadAirportsList() {
 
     let request = new XMLHttpRequest();
+    request.open("GET", "https://raw.githubusercontent.com/dace/Airports-Countries-JSON/master/airports_countries.json", true);
+    request.send();
 
     request.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            showAirportsList(JSON.parse(this.responseText));
-            arrayAirportsList.push(JSON.parse(this.responseText));
+            showCountryList(JSON.parse(this.responseText));
+            airportsList.push(JSON.parse(this.responseText));
         }
     }
-    request.open("GET", "https://api.duffel.com/air/airports", true);
-    request.send();
 }
 
-function showAirportsList(airports) {
-    console.log('full airports array');
-    console.log(airports);
-    airports.data.forEach(airport => {
-        console.log('airport element');
-        console.log(airport);
-        if(airport.time_zone.toLowerCase().includes('europe')) {
-            airportsEurope.push(airport);
+let countryList = [];
+
+function showCountryList(resultatRequete) {
+    resultatRequete.forEach(object => {
+        if (!countryList.includes(object.countryName)) {
+            countryList.push(object.countryName);
         }
     })
+    createCountrySelect(countryList);
 }
-console.log('europe airports array');
-console.log(airportsEurope);
 
 
+
+// Variables pour récupérer les selects en HTML
+let departList = document.getElementById('depart-list');
+let arrivalList = document.getElementById('arrival-list');
+
+// Fonction qui va afficher la liste des pays et des aéroports dans les deux select
+function createCountrySelect(list) {
+    // Pour CHAQUE PAYS on crée un élément "option" dans le select des pays. Et on lui ajoute un écouteur
+    list.forEach(country => {
+        const countryElement = document.createElement('option');
+        countryElement.innerHTML = country;
+        departList.appendChild(countryElement);
+        // L'écouteur permettra de vérifier quel pays est cliqué, et vérifie le tableau des aéroports pour afficher CHAQUE aéroports du pays cliqué, et crée les 
+        // options dans le deuxième select
+        countryElement.addEventListener('click', () => {
+            // On réinitialise l'affichage du second select, sinon, chaque clic sur un pays gardera la liste affichée, et on veux la changer pour chaque pays.
+            arrivalList.innerHTML = "<option selected>Aéroports Disponibles</option>";
+            for (i = 0; i < airportsList[0].length; i++) {
+                // on vérifie pour chaque objet si le nom du pays correspond à celui qu'on a cliqué, pour ensuite crée les éléments et les afficher
+                if (airportsList[0][i].countryName.includes(country)) {
+                    const airportElement = document.createElement('option');
+                    airportElement.innerHTML = airportsList[0][i].airportName;
+                    arrivalList.appendChild(airportElement);
+                }
+            }
+        })
+    })
+}
 
 
 
@@ -146,11 +174,11 @@ console.log(airportsEurope);
 
 
     // console.log(json);
-    
-    
+
+
     // function airportsList(){
-       
-    
+
+
     //   for (x=0; x<json.length;x++){
     //       if(json[x].continent=='EU'){
     //           airportsEurope.push(json[x]);
@@ -158,5 +186,5 @@ console.log(airportsEurope);
     //   }
     //   console.log(airportsEurope);
     // }
-      
+
     // loadJSON();
